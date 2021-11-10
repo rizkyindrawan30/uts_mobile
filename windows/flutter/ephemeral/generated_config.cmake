0c9ae3a3053df74cf049202a0fa939fc7e8d1a46
rizkyindrawan30 <71561019+rizkyindrawan30@users.noreply.github.com>
2021-11-10 20:07:37 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm"
+  "FLUTTER_TARGET=D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Rizky Indrawan\\Kampus\\Manajemen Informatka\\Semester 5\\Pemrograman Mobile\\Prektek\\latihan_mvvm\\.dart_tool\\package_config.json"
+)
